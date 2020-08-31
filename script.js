c31abbcf06e894b236ae376afc4407b3d63e891a
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-08-31 13:25:49 +0200

    fn idGen

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
             /////  HW#13 Generators  /////
+
+const idGenerator = createIdGenerator();
+let id = 1;
+
+function* createIdGenerator() {
+  while (true) {
+    yield id;
+    id++;
+  }
+}
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
