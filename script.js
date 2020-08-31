d6431b001e239fcbc1d06dc1dd9ea6d3012180ba
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-08-31 15:31:42 +0200

    dead end, need rest

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
             /////  HW#13 Generators  /////
 
+// Id Generator Function
 const idGenerator = createIdGenerator();
 let id = 1;
 
@@ -10,9 +11,26 @@ function* createIdGenerator() {
   }
 }
 
+// Id Generator connect with Document
 const getId = document.getElementById('id')
 const idNum = document.getElementById('num')
 
 getId.addEventListener('click', function () {
   idNum.innerText = idGenerator.next().value
+})
+
+// Change Font Size Function
+const docText = document.getElementById('sizeText')
+const fontGenerator = newFontSizeGenerator(14);
+
+function newFontSizeGenerator(size) {
+  docText.style.fontSize = fontGerneator.next().value + 'px'
+}
+
+minus.addEventListener('click', function() {
+  docText.style.fontSize = fontGerneator.next().value + 'px;'
+})
+
+plus.addEventListener('click', function() {
+  docText.style.fontSize = fontGenerator.next().value + 'px;'
 })
