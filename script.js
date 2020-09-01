4817486228d007bcbb8ab4c05a8573f964f7d7d0
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-09-02 00:35:42 +0200

    only idGen

--- script.js
+++ script.js
@@ -18,27 +18,3 @@ const idNum = document.getElementById('num')
 getId.addEventListener('click', function () {
   idNum.innerText = idGenerator.next().value
 })
-//
-// // Change Font Size Function
-// const fontGenerator = newFontSizeGenerator(14);
-// let target = '';
-//
-// function newFontSizeGenerator(size) {
-//   while (true) {
-//     if
-//   }
-//   docText.style.fontSize = fontGerneator.next().value + 'px'
-// }
-
-
-
-
-// const docText = document.getElementById('sizeText')
-//
-// minus.addEventListener('click', function() {
-//   docText.style.fontSize = fontGerneator.next('-').value + 'px;'
-// })
-//
-// plus.addEventListener('click', function() {
-//   docText.style.fontSize = fontGenerator.next('+').value + 'px;'
-// })
